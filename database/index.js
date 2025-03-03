9add9d5c4b7c009b6ca68e6ebf6b7f612a40d750
coder4al <coder4al@gmail.com>
2025-03-03 12:05:10 +0530

    removed depricated : useNewUrlParser, useUnifiedTopology from index.js

--- database/index.js
+++ database/index.js
@@ -4,10 +4,7 @@ require("dotenv").config();
 // Function to connect to MongoDB
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDb");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
